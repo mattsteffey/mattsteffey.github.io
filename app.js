c09fd4902f2530c0251cf40b9d0d5346ff9cd1fe
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-05-15 17:04:40 -0600

    cool

--- app.js
+++ app.js
@@ -9,13 +9,77 @@ async function go() {
   video.srcObject = stream;
 }
 
+
 function takePhoto() {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  const data = canvas.toDataURL('image/jpeg');
-  console.log(data);
-}
+  const data = canvas.toDataURL('image/png');
+  var res = data.replace("data:image/png;base64,", "");
+ 
+ var type = "WEB_DETECTION";
+ var json = '{' +
+    ' "requests": [' +
+    ' { ' +
+    '   "image": {' +
+    '     "content":"' + res + '"' +
+    '   },' +
+    '   "features": [' +
+    '       {' +
+    '         "type": "' + type + '",' +
+    '     "maxResults": 1' +
+    '       }' +
+    '   ]' +
+    ' }' +
+    ']' +
+    '}';
+
+$.ajax({
+    type: 'POST',
+    url: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyD1h9v1qDj0EMx6UjLtpFn5t0ZjPGxstSQ",
+    dataType: 'json',
+    data: json,
+    //Include headers, otherwise you get an odd 400 error.
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    success: function(data, textStatus, jqXHR) {
+      musicInfo = data.responses["0"].webDetection.bestGuessLabels["0"].label;
+      $.ajax({
+        method: "GET",
+        url: "https://api.spotify.com/v1/search?q=" + musicInfo + "&type=album&limit=1",
+        headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer BQBjBIT1lMeGA5_37wWqndCmGLqQ93O0kVyvZ_ZubqwUIpTNc_q_5dCsONFB8WZ4Gg3fAiPpLbLzCM8IJdbxOEOhb50paTd2dn9KQuaKp0-XI8gLw799D2mUPepNWwIoKgAY1EUPQ7Bj9c4'
+},
+          }).done(function(data) {
+             window.open(data.albums.items["0"].external_urls.spotify,'_blank');
+             console.log(data.albums.items["0"]);
+
+
+
+
+             // var albumURL = document.getElementById('player');
+             // albumURL.setAttribute("src", 'https://open.spotify.com/embed?uri='+ data.albums.items["0"].uri);
+             // console.log(data.albums.items["0"].uri);
+
+          });
+
+      console.log(musicInfo);
+
+
+// spotify:album:42oggZmZI9b5BOPMGvYuXh
+
+  },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log('ERRORS: ' + textStatus + ' ' + errorThrown);
+    }
+  });   
+
+
+    }
+
 
 function deletePhoto() {
   var can = document.getElementById("paint");
@@ -26,3 +90,4 @@ function deletePhoto() {
 go().catch(err => {
   alert(err.message);
 });
+
